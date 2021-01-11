c205e92586f67fda836cd8dc5a342aab32ca7ddb
Travis Howse <tjhowse@gmail.com>
2021-01-11 14:13:36 +1000

    Basically working.

--- gb2influxdb/__init__.py
+++ gb2influxdb/__init__.py
@@ -18,17 +18,20 @@ DEFAULT_API_PARAMS["epoch"] = "s"
 DEFAULT_API_PARAMS["db"] = INFLUXDB_DB_NAME
 
 class gb2influxdb():
-    def __init__(self, uri, username, password, database):
+    def __init__(self, uri, username, password, database, force, oneshot):
         self._uri = uri
         if not self._uri.endswith("/"):
             self._uri += "/"
         self._username = username
         self._password = password
         self._database = database
+        self._force = force
+        self._oneshot = oneshot
         self._db_schema = self._get_gb_db_schema()
-        print(self._db_schema)
+        # print(self._db_schema[GADGETBRIDGE_DB_TABLE])
 
     def setup(self):
+        # result = self._do_post("DROP DATABASE gadgetbridge").json()
         try:
             result = self._do_post("CREATE DATABASE gadgetbridge").json()
             if result['results'][0]['statement_id'] != 0:
@@ -39,20 +42,23 @@ class gb2influxdb():
         # print(self._get_gb_db_rows(GADGETBRIDGE_DB_TABLE, 1610320440))
         self._process_tables()
 
+        # result = self._do_get("SELECT * FROM {} ORDER BY time".format(GADGETBRIDGE_DB_TABLE)).json()
+        # print(result)
+
     def _process_tables(self):
         for table in self._db_schema.keys():
             line_protocol = ''
             if 'timestamp' != self._db_schema[table][0].lower():
-                print("Skipping syncing table {} with schema {}.".format(table, self._db_schema[table]))
+                logging.debug("Skipping syncing table {} with schema {}.".format(table, self._db_schema[table]))
                 continue
             last_time = self._get_last_influxdb_timestamp_in_measurement(table)
             new_rows = self._get_gb_db_rows(table, last_time)
+            logging.info("Syncing {} entries into measurement {}".format(len(new_rows), table))
 
             for row in new_rows:
                 line_protocol += self._convert_row_to_influxdb_line(table, row)
             # TODO Batch these max 5k
             self._do_write(line_protocol)
-        # print(line_protocol)
 
     def _convert_row_to_influxdb_line(self, table, row):
         column_names = self._db_schema[table]
@@ -70,6 +76,9 @@ class gb2influxdb():
 
     def _get_last_influxdb_timestamp_in_measurement(self, meas):
         # This queries influxdb for the last timestamp we wrote to it in unix time
+        if self._force:
+            # Sync everything, not just data since the last sync.
+            return 0
         result = self._do_get("SELECT * FROM {} ORDER BY time DESC LIMIT 1".format(meas)).json()
         try:
             return result['results'][0]['series'][0]['values'][0][0]
@@ -122,6 +131,7 @@ class gb2influxdb():
 
 
     def loop_forever(self):
+        # Watch for a change on the DB file. Wait a few seconds. Reload it.
         pass
         # result = self._do_post("CREATE DATABASE gadgetbridge").json()
         # result = self._do_post("DROP DATABASE gadgetbridge").json()
@@ -144,14 +154,15 @@ class gb2influxdb():
 @click.option('--username', default='', help='The username to authenticate to the InfluxDB server.', show_default=True)
 @click.option('--password', default='', help='The password to authenticate to the InfluxDB server.', show_default=True)
 @click.option('--database', default='./gbdb.sqlite3', help='The database file to sync from.', show_default=True)
-# @click.option('--interval', default=60, help='The update interval in minutes.', show_default=True)
-def main(uri, username, password, database):
+@click.option('--force', default=False, help='Force a full historical sync rather than only sync new data.', show_default=True)
+@click.option('--oneshot', default=False, help='Do one sync and quit. Don\'t monitor the database for changes.', show_default=True)
+def main(uri, username, password, database, force, oneshot):
     logging.basicConfig(
         format='%(asctime)s %(levelname)-8s %(message)s',
         level=logging.INFO,
         datefmt='%Y-%m-%d %H:%M:%S')
     logging.info("Starting gb2influxdb v{}".format(version.version))
-    i = gb2influxdb(uri, username, password, database)
+    i = gb2influxdb(uri, username, password, database, force, oneshot)
     i.setup()
     i.loop_forever()
 
